9e092f1913ec609d4f9dd591644b539a40ac630d
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-10-30 10:28:16 +0000

    complete function 1 to get password options from the user and store it in an object

--- script.js
+++ script.js
@@ -90,7 +90,57 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Prompt for password length
+  var length = parseInt(prompt("Enter the password length (between 8 and 128 characters inclusive):"));
 
+  // Check if the length is a valid number
+  /* 
+    isNan checks if the input is a number.
+    If the input is less than 8 characters or grater than 128 characters,
+    then the if statement returns null, which means nothing has been return.
+  */
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Please enter a valid number between 8 and 128.");
+    return null;
+  }
+
+  // Confirm statements for character types
+  /*
+    A series of confirm statements for the user to decide what
+    type of characters they would like in their password.
+  */
+  var specialChar = confirm("Would you like to include special characters?");
+  var numChar = confirm("Would you like to include numeric characters?");
+  var lowerChar = confirm("Would you like to include lowercase characters?");
+  var upperChar = confirm("Would you like to include uppercase characters?");
+
+  // Check if at least one character type is selected
+  /*
+    For the password generator to work, at least one character
+    needs to be selected. If the user rejects all character types,
+    nothing will be returned.
+  */
+  if (!specialChar && !numChar && !lowerChar && !upperChar) {
+    alert("Please select at least one character type.");
+    return null;
+  }
+
+  // Store user choices in an object
+  /*
+    In order to access the choices the user selectes, we must
+    store them in an object, so that we can access these values
+    later on. Returning the passwordOptions, will allow us to do
+    that.
+  */
+  var passwordOptions = {
+    length: length,
+    specialChar: specialChar,
+    numChar: numChar,
+    lowerChar: lowerChar,
+    upperChar: upperChar
+  };
+
+  return passwordOptions;
 }
 
 // Function for getting a random element from an array
